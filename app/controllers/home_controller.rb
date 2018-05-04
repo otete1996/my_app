2bcc5d3f70952e8f1b27a4f88358477be96d1d70
我妻　伶治 <otete1996@yahoo.co.jp>
2018-05-04 16:37:42 +0900

    mai
    create main

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,7 @@ class HomeController < ApplicationController
   def top
  @texts=Article.all
   end
+  def main
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/top'
-  root "home#top"
+  get '/index'=>"home#top"
+  root "home#main"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
