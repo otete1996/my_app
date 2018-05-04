7f0e8a8a0f227986123830c97e7561ba20741b24
我妻　伶治 <otete1996@yahoo.co.jp>
2018-05-04 12:14:53 +0900

    bootstrap適用

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,22 @@
 class HomeController < ApplicationController
+
   def top
+    require 'open-uri'
+require 'nokogiri'
+
+url = 'https://news.yahoo.co.jp/list/?d=20180504&mc=f&mp=f'
+
+charset = nil
+html = open(url) do |f|
+  charset = f.charset
+  f.read
+end
+
+page = Nokogiri::HTML.parse(html, nil, charset)
+
+shuzo_meigen = page.css("dt")[0]
+
+  @txt=shuzo_meigen.text
+
   end
 end
