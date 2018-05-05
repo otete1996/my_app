3cb80fa0e9976a51915c066430efe5bd832c8763
我妻　伶治 <otete1996@yahoo.co.jp>
2018-05-05 14:25:03 +0900

    ログアウト機能とpostcontrollerへのアクセス制限

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,17 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :correct_user,only: [:destroy]
   def create
     @post=Post.new(post_params)
     if @post.save
-      redirect_to(article_path(@post.article))
+      redirect_to(article_path(@post.article),notice: '投稿に成功しました')
     else
       render(article_path(@post.article))
     end
   end
 
   def edit
+    @post=Post.find(params[:id])
   end
 
   def update
@@ -17,12 +20,20 @@ class PostsController < ApplicationController
   def destroy
     @post=Post.find(params[:id])
     @post.destroy
-    redirect_to(article_path(@post.article))
+    redirect_to(article_path(@post.article),notice: "削除しました")
   end
 
   private
   def post_params
     params.require(:post).permit(:content,:user_id,:article_id)
+  end
+
+  def correct_user
+    @post=Post.find(params[:id])
+    if current_user.id==@post.user_id
+    else
+      redirect_to(articles_path,notice:"権限がありません")
+    end
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :posts
   devise_for :users
+  resources :posts
+
   #devise_for :users
   resources :articles,{only: [:show]}
 
   get '/index'=>"home#top"
   root "home#main"
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
