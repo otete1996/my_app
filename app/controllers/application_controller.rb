25e4aacc0f2ce2d2f842228a3a95b5bd1216cd93
我妻　伶治 <otete1996@yahoo.co.jp>
2018-05-05 18:37:30 +0900

    リファクタリング前

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
 class ApplicationController < ActionController::Base
+  require 'date'
+  require 'open-uri'
+  require 'nokogiri'
   protect_from_forgery with: :exception
   before_action :set_up
   before_action :set_up2
   private
 
   def set_up
-    require 'date'
-    require 'open-uri'
-    require 'nokogiri'
+    # require 'date'
+    # require 'open-uri'
+    # require 'nokogiri'
     day = Date.today
     article=Article.last
     if article.blank? || article.date!=day
@@ -34,32 +37,53 @@ class ApplicationController < ActionController::Base
     end
   end
   def set_up2
-    require 'date'
-    require 'open-uri'
-    require 'nokogiri'
+    # require 'date'
+    # require 'open-uri'
+    # require 'nokogiri'
     day = Date.today
     topic=Article2.last
     if topic.blank? || topic.date!=day
-      url = 'https://mainichi.jp/flash/1'
+      # url = 'https://mainichi.jp/flash/1'
+      ccccc('https://mainichi.jp/flash/1', ".midashi"))
 
-      charset = nil
-      html = open(url) do |f|
-        charset = f.charset
-        f.read
-      end
+      # charset = nil
+      # html = open(url) do |f|
+      #   charset = f.charset
+      #   f.read
+      # end
+      #
+      # @title=Array.new(20)
+      # page = Nokogiri::HTML.parse(html, nil, charset)
+      #
+      # 20.times do |index|
+      #   shuzo_meigen = page.css(".midashi")[index]
+      #   @title[index]=shuzo_meigen.text
+      # end
+      #
+      # 20.times do |index|
+      #   @post2=Article2.new(content: @title[index],date: day)
+      #   @post2.save
+      # end
+    end
+  end
+  def ccccc(url, css_test)
+    charset = nil
+    html = open(url) do |f|
+      charset = f.charset
+      f.read
+    end
 
-      @title=Array.new(20)
-      page = Nokogiri::HTML.parse(html, nil, charset)
+    @title=Array.new(20)
+    page = Nokogiri::HTML.parse(html, nil, charset)
 
-      20.times do |index|
-        shuzo_meigen = page.css(".midashi")[index]
-        @title[index]=shuzo_meigen.text
-      end
+    20.times do |index|
+      shuzo_meigen = page.css(css_test)[index]
+      @title[index]=shuzo_meigen.text
+    end
 
-      20.times do |index|
-        @post2=Article2.new(content: @title[index],date: day)
-        @post2.save
-      end
+    20.times do |index|
+      @post2=Article2.new(content: @title[index],date: day)
+      @post2.save
     end
   end
 end
--- app/controllers/posts2_controller.rb
+++ app/controllers/posts2_controller.rb
@@ -1,8 +1,22 @@
 class Posts2Controller < ApplicationController
+  before_action :authenticate_user!
+  before_action :correct_user,only: [:destroy]
   def create
+    @post=Post2.new(post2_params)
+    if @post.save
+      redirect_to(article2_path(@post.article),notice: '投稿に成功しました')
+    else
+      @posts=Post2.where(article_id: params[:article2_id].to_i)
+      @article=Article2.find(params[:article_id])
+      flash[:alert]="項目を入力してください"
+      render template: "articles/show"
+    end
   end
 
   def destroy
+    @post=Post.find(params[:id])
+    @post.destroy
+    redirect_to(article2_path(@post.article),notice: "削除しました")
   end
 
   def edit
@@ -10,4 +24,16 @@ class Posts2Controller < ApplicationController
 
   def update
   end
+  private
+  def post2_params
+    params.require(:post2).permit(:content,:user_id,:article_id)
+  end
+  def correct_user
+    @post=Post.find(params[:id])
+    if current_user.id==@post.user_id
+    else
+      redirect_to(articles_path,notice:"権限がありません")
+    end
+  end
+
 end
